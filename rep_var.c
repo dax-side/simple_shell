e7ee8bbce2c28ca889788be92cb7d6d238c4b6fe
dax-side <damade615@gmail.com>
2022-06-13 13:31:18 -0700

    new updated commit

---
+++ rep_var.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * check_env - checks if the typed variable is an env variable
+ *
+ * @h: head of linked list
+ * @in: input string
+ * @data: data structure
+ * Return: no return
+ */
+void check_env(r_var **h, char *in, data_shell *data)
+{
+	int row, chr, j, lval;
+	char **_envr;
+
+	_envr = data->_environ;
+	for (row = 0; _envr[row]; row++)
+	{
+		for (j = 1, chr = 0; _envr[row][chr]; chr++)
+			
